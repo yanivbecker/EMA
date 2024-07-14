a760326eae307708837a119e243ffaacc7a078b7
Yaniv Becker <yanivbecker@gmail.com>
2024-07-14 20:22:27 +0300

    initial commit

---
+++ data/story.js
@@ -0,0 +1,252 @@
+var story = {
+ "docName": "EMA",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "EMA",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "62:333",
+   "groupIndex": 0,
+   "title": "EMA v2",
+   "image": "ema-v2.png",
+   "index": 0,
+   "width": 384,
+   "height": 844,
+   "x": 809,
+   "y": 1885,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Extended FAB",
+     "rect": {
+      "x": 98,
+      "y": 646,
+      "width": 189,
+      "height": 52
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "54:47",
+   "groupIndex": 0,
+   "title": "emergency v2",
+   "image": "emergency-v2.png",
+   "index": 1,
+   "width": 384,
+   "height": 844,
+   "x": 1233,
+   "y": 1885,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "navigation/menu - wallet",
+     "rect": {
+      "x": 185.6666259765625,
+      "y": 758,
+      "width": 75,
+      "height": 75
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "navigation/menu - analysis",
+     "rect": {
+      "x": 283.3333740234375,
+      "y": 759,
+      "width": 73,
+      "height": 73
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 1,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "56:65",
+   "groupIndex": 0,
+   "title": "contacts v2",
+   "image": "contacts-v2.png",
+   "index": 2,
+   "width": 384,
+   "height": 844,
+   "x": 1657,
+   "y": 1885,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "navigation/menu - analysis",
+     "rect": {
+      "x": 302.3333740234375,
+      "y": 754,
+      "width": 73,
+      "height": 73
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "BNB-11",
+     "rect": {
+      "x": 39,
+      "y": 753,
+      "width": 428,
+      "height": 75
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  },
+  {
+   "id": "56:69",
+   "groupIndex": 0,
+   "title": "what happend v2",
+   "image": "what-happend-v2.png",
+   "index": 3,
+   "width": 384,
+   "height": 844,
+   "x": 2081,
+   "y": 1885,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "navigation/menu - home",
+     "rect": {
+      "x": 43,
+      "y": 756,
+      "width": 75,
+      "height": 75
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 1,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    },
+    {
+     "name": "navigation/menu - wallet",
+     "rect": {
+      "x": 130.333251953125,
+      "y": 756,
+      "width": 75,
+      "height": 75
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 0.30000001192092896
+      }
+     ]
+    }
+   ],
+   "layout": null,
+   "tmpSolitBackground": false
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 4,
+ "fileKey": "ema1",
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#C4C4C4"
+  }
+ ]
+}
